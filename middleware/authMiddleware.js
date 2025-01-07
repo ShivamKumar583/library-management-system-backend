f8cec0c703ff253c5a69a4fb9b5bcc08fb4a5cdb
ShivamKumar583 <shivam.kumar9910506804@gmail.com>
2025-01-08 00:09:49 +0530

    Update middleware code-2

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 exports.protect = (req, res, next) => {
     const token = req.headers.authorization;
-    console.log(req.headers.authorization);
     if (!token) return res.status(401).json({ message: 'Not authorized,login in first.' });
 
     try {
