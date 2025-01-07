03942c195dd10ab77a71b145a302cb9c3db570f5
ShivamKumar583 <shivam.kumar9910506804@gmail.com>
2025-01-07 12:24:15 +0530

    Updating routes code-2

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { getAllBooks, addBook, deleteBook, borrowBook, returnBook, getAllBooksByAvailability, getTotalNumberOfBooks } = require('../controllers/bookController');
+const { protect, isAdmin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/getAllBooks', getAllBooks);
-router.get('/getAllBooksByAvailability', getAllBooksByAvailability);
-router.post('/add/:id', addBook);
-router.delete('/delete/:id', deleteBook);
-router.post('/borrow/:id', borrowBook);
-router.post('/return/:id', returnBook);
-router.get('/getTotalNumberOfBooks', getTotalNumberOfBooks);
+router.get('/getAllBooks',protect, getAllBooks);
+router.get('/getAllBooksByAvailability',protect, getAllBooksByAvailability);
+router.post('/add/:id',protect,isAdmin, addBook);
+router.delete('/delete/:id',protect,isAdmin, deleteBook);
+router.post('/borrow/:id',protect, borrowBook);
+router.post('/return/:id',protect, returnBook);
+router.get('/getTotalNumberOfBooks',protect, getTotalNumberOfBooks);
 
 module.exports = router;
