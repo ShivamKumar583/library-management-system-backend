633cade732be0e7316f6a580be82d4d30df153ce
ShivamKumar583 <shivam.kumar9910506804@gmail.com>
2025-01-07 12:25:16 +0530

    Adding middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+exports.protect = (req, res, next) => {
+    const token = req.headers.authorization;
+    console.log(req.headers.authorization);
+    if (!token) return res.status(401).json({ message: 'Not authorized,login in first.' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+exports.isAdmin = (req, res, next) => {
+    if (!req.user.isAdmin) return res.status(403).json({ message: 'Admin access required' });
+    next();
+};
