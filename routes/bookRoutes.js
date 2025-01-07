93e04e5629ca5d54e9be088eb5b57d312f8616b8
ShivamKumar583 <shivam.kumar9910506804@gmail.com>
2025-01-08 00:11:21 +0530

    Update routes code-3

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const { getAllBooks, addBook, deleteBook, borrowBook, returnBook, getAllBooksByAvailability, getTotalNumberOfBooks } = require('../controllers/bookController');
+const { getAllBooks, addBook, deleteBook, borrowBook, returnBook, getAllBooksByAvailability, getTotalNumberOfBooks, getAllBooksBorrowedByUser, getLastTransactionByUser } = require('../controllers/bookController');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
+// admin 
 router.get('/getAllBooks',protect, getAllBooks);
-router.get('/getAllBooksByAvailability',protect, getAllBooksByAvailability);
-router.post('/add/:id',protect,isAdmin, addBook);
-router.delete('/delete/:id',protect,isAdmin, deleteBook);
-router.post('/borrow/:id',protect, borrowBook);
-router.post('/return/:id',protect, returnBook);
+router.post('/add',protect,isAdmin, addBook);
+router.delete('/delete/:bookId',protect,isAdmin, deleteBook);
 router.get('/getTotalNumberOfBooks',protect, getTotalNumberOfBooks);
+
+// normal user
+router.get('/borrowedByUser/:userId',protect, getAllBooksBorrowedByUser);
+router.get("/transactions/last/:userId",protect, getLastTransactionByUser);
+router.post('/borrow/:id',protect, borrowBook);
+router.post('/return/:bookId',protect, returnBook);
+router.get('/getAllBooksByAvailability',protect, getAllBooksByAvailability);
 
 module.exports = router;
